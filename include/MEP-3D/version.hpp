3fec2ee0e8f4a5296f493eb4bff6b57143d162b9
Piotr Skibinski <pskib00@gmail.com>
2023-08-11 21:55:35 +0200

    Volume-Rendering: Update version to 2.1.2

--- include/MEP-3D/version.hpp
+++ include/MEP-3D/version.hpp
@@ -2,7 +2,7 @@
 #define MEP_VERSION_H
 #define MEP_MAJOR 2
 #define MEP_MINOR 1
-#define MEP_PATCH 1
-#define MEP_SUFFIX ".rc1"
-#define MEP_FULL_VERSION "2.1.1.rc1"
+#define MEP_PATCH 2
+#define MEP_SUFFIX ""
+#define MEP_FULL_VERSION "2.1.2"
 #endif /* FOOBAR_VERSION_H_ */
